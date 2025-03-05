a15dddb2e7ed958da36fbcc864ccf23ed11bbc42
Maekee <drmicke@gmail.com>
2025-03-05 17:17:42 +0100

    Update StringIsTheThing.cs

--- Learning/StringIsTheThing.cs
+++ Learning/StringIsTheThing.cs
@@ -1,1 +1,38 @@
+// Strings are immutable, meaning you re-create them in the background of you modify them.
+// If there are a lot of string modifications, we can improve performance with StringBuilder.
 
+StringBuilder sb = new StringBuilder();
+
+sb.Append("123");
+sb.Append("45");
+sb.Replace("12345", "ABCDE");
+sb.Clear();
+
+Console.Write(sb.ToString());
+
+// A string is a reference type, but often behaves like a value type. C# makes string easy to create with the equal operator.
+// Behind the scenes a constructor is used, which we also can explicitly call
+
+string ordinaryHello = new string("Hello");
+string fromCharArray = new string(charArray);
+
+// Other nice things we can do with strings...
+string formatted = string.Format("The value is: {0}", 42); //Composite formatting
+string interpolated = $"The value is: {42}"; //String Interpolation
+string filePath = @"C:\Windows\System32\calc.exe" //Verbatim string literal
+string rawStringOneLine = """This is a "raw string literal". It can contain characters like \, ' and ".""" //Raw string literal, starts and ends with """
+string rawStringMultipleLines = """
+        <xml example = "true">
+            <body>
+            </body>
+        </xml>
+        """;
+
+double doubleValue = 1632.54;
+doubleValue.ToString("C", new System.Globalization.CultureInfo("en-US")) //$1,632.54
+
+42.ToString("X") //Hex 2A
+42.ToString("B") //Binary 101010
+42.ToString("D5") //Digits 00042
+42.ToString("F2") //Decimals 42,00
+0.1.ToString("P1") //Percent 10,0%
