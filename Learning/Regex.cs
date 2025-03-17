cd5ddcc5d0e59b7eb394fdbbed376cfa67e20110
Maekee <drmicke@gmail.com>
2025-03-17 16:12:45 +0100

    Update Regex.cs

--- Learning/Regex.cs
+++ Learning/Regex.cs
@@ -1,16 +1,16 @@
 // I am a big fan of regex, so here goes...
 
-// Regex.IsMatch //IsMatch returns bool
+////// Regex.IsMatch //IsMatch returns bool //////
 Regex.IsMatch("Uppsala","""U\w+"""); //Returns true
 Regex.IsMatch("uppsala","""U\w+"""); //Returns false (case sensitive)
 Regex.IsMatch("uppsala","""U\w+""",RegexOptions.IgnoreCase); //Returns true (case insensitive)
 
-// Regex.Replace
+////// Regex.Replace //////
 string regexFilter = """(S\w+)""";
 string originalString = "Victoria plays with Steve";
 string newString = Regex.Replace(originalString, regexFilter, "Herobrine"); //Returns "Victoria plays with Herobrine"
 
-// Regex.Split
+////// Regex.Split //////
 string input = @"07/14-2007";
 string patternCaptureGroups = @"(-)|(/)";
 string patternNoCaptureGroups = @"-|/";
@@ -18,7 +18,7 @@ string patternNoCaptureGroups = @"-|/";
 string[] here = Regex.Split(input, patternCaptureGroups); //Returns string array with 07, /, 14, -, 2007 (regex using capture groups)
 string[] here = Regex.Split(input, patternNoCaptureGroups); //Returns string array with 07, 14, 2007 (regex using no capture groups)
 
-// Regex.Match code example with capture group
+////// Regex.Match code example with capture group //////
 string input = @"07/14/2007";
 string pattern = @"\d+/\d+/(\d+)$";
 
@@ -29,7 +29,7 @@ if (regexMatch.Success)
     string matchValue = regexMatch.Groups[1].Value;
 }
 
-// Regex.Matches code example that captures all matches
+////// Regex.Matches code example that captures all matches //////
 string input = "Monday is the first day, Tuesday is the second and Wednesday third";
 string regexFilter = @"(\w{3,6}day)";
 
@@ -43,9 +43,9 @@ foreach (var regexMatch in result)
     Console.WriteLine(regexMatch);
 }
 
-//Code example using IsMatch
+////// Code example using IsMatch //////
 
-//How about matching from string array, and make it case-insensitive
+// How about matching from string array, and make it case-insensitive
 string[] swedishCitiesArr = "Visby,Stockholm,Malmö,Göteborg,Uppsala,Västerås,Umeå,Kalmar".Split(",");
 string regexFilter = """(V\w+|U\w+)""";
 
@@ -65,5 +65,5 @@ foreach (string city in swedishCitiesArr.Order())
     }
 }
 
-//We can also use my dear friend System.Linq, case insensitive
+// We can also use my dear friend System.Linq, case insensitive
 var matches = swedishCitiesArr.Where(city => Regex.IsMatch(city, """(V\w+|U\w+)""", RegexOptions.IgnoreCase));
