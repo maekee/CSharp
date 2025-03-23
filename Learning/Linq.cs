a7f61ca79202143edb1dc63dd2f69c2130345b4a
Maekee <drmicke@gmail.com>
2025-03-23 18:15:35 +0100

    Update Linq.cs

--- Learning/Linq.cs
+++ Learning/Linq.cs
@@ -33,3 +33,23 @@ newList = (from ghost in ghosts where ghost.GhostName.StartsWith("C") && ghost.G
 // Where, method syntax
 newList = ghosts.Where(ghost => ghost.GhostName.StartsWith("C") && ghost.GhostAge < 500).ToList();
 newList = ghosts.Where(ghost => Regex.Match(ghost.GhostName, @"^\w+er").Success).ToList();
+
+// FIRST, FIRSTORDEFAULT, LAST, LASTORDEFAULT, SINGLE, SINGLEORDEFAULT examples
+
+// First, query syntax followed by method syntax. Throws exception, so remember to catch these
+resultGhost = (from ghost in ghosts select ghost).First(ghost => ghost.GhostColor == "Yellow");
+resultGhost = ghosts.First(ghost => ghost.GhostColor == "Yellow");
+
+// FirstOrDefault, query syntax followed by method syntax. Returns null of no match
+resultGhost = (from ghost in ghosts select ghost).FirstOrDefault(ghost => ghost.GhostColor == "Yellow");
+resultGhost = ghosts.FirstOrDefault(ghost => ghost.GhostColor == "Yellow");
+// FirstOrDefault can have a second parameter, that is a new class object returned if nothing is found instead of null
+resultGhost = ghosts.FirstOrDefault(ghost => ghost.GhostColor == "Yellowish",
+    new Ghost("MrNothing"));
+    
+// Last and LastOrDefault, works the same but searches backwards. Just like First, Last also Throws exception, so remember to catch these
+
+// Single, returns of a single one item is found (throws exception if not found or multiple found) 
+// SingleOrDefault, returns null instead of exception. Like the other ...OrDefault methods.
+
+// Just like the FirstOrDefault, both LastOrDefault and SingleOrDefault can also take a second parameter
