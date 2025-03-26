7ee348948ac8a5ead40563c4b2f34694eb722e20
Maekee <drmicke@gmail.com>
2025-03-26 10:08:46 +0100

    Update Linq.cs

--- Learning/Linq.cs
+++ Learning/Linq.cs
@@ -138,3 +138,23 @@ boolResult = myIntList.Contains("four");
 
 // In essence, Contains is a more precise check for a exact value,
 // while Any is a more flexible check for the existence of elements that meet a condition.
+
+// Contains with custom class Objects, we need to create a new class that inherits from EqualityComparer<T> class
+// Within that new class we need to override the Equals and GetHashCode methods with logic that meet our requirements.
+
+public class GhostNameComparer : EqualityComparer<Ghost>
+{
+    public override bool Equals(Ghost x, Ghost y)
+    {
+        return (x.GhostName == y.GhostName);
+    }
+    public override int GetHashCode(Ghost obj) // Have to return a unique value for each object
+    {
+        return obj.GhostName.GetHashCode();
+    }
+}
+
+// Then we can use the Contains method and passing in a object of GhostNameComparer
+GhostNameComparer ghostComparer = new();
+
+boolResult = ghosts.Contains(new Ghost("Casper"), ghostComparer); // Make sure there is a constructor that takes the parameters passed to the new object
