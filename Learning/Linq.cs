141a5dcc0d464ea0ae5013caf188282b7a7053d4
Maekee <drmicke@gmail.com>
2025-03-26 09:46:53 +0100

    Update Linq.cs

--- Learning/Linq.cs
+++ Learning/Linq.cs
@@ -114,3 +114,27 @@ newGhostList = (from ghost in ghosts
                 select ghost).Chunk(10).ToList();
 
 newGhostList = ghosts.OrderBy(ghost => ghost.GhostAge).Chunk(25).ToList();
+
+
+// ALL/ANY
+// Do All or Any items meet a condition? Returns bool
+// IEnumerable<T>.All(predicate) //Does if all items meet the condition
+// IEnumerable<T>.Any(predicate) //Does if any item meets the condition
+
+boolResult = ghosts.All(ghost => ghost.GhostName == ghost.GhostColor);
+boolResult = (from ghost in ghosts select ghost).Any(ghost => ghost.GhostName == ghost.GhostColor);
+
+
+// CONTAINS
+// Contains() returns true based on exact presence of a specific value
+// Also works on strings but is case sensitive (leverages the value-based equality of strings, so no need to worry about the reference equality issues)
+// Uses direct equality, so class objects use the default equality comparer, this
+// comparer is based on the object's reference, not the object's value.
+// This means that Contains will return true only if the object in the collection
+// is the exact same object instance as the one you're checking for.
+
+var myIntList = new List<string> { "one", "two", "three" };
+boolResult = myIntList.Contains("four");
+
+// In essence, Contains is a more precise check for a exact value,
+// while Any is a more flexible check for the existence of elements that meet a condition.
