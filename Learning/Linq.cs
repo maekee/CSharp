de1b249d528b9b1a45418f307e20ce697c91f292
Maekee <drmicke@gmail.com>
2025-04-09 19:02:20 +0200

    Update Linq.cs

--- Learning/Linq.cs
+++ Learning/Linq.cs
@@ -214,12 +214,18 @@ IEnumerable<Ghost> diff = ghostsList1.ExceptBy<Ghost, int>(
 //   Compare GhostAge values from ghostsList1 with GhostAge values from ghostsList2
 
 
-// INTERSECT
+// INTERSECT/INTERSECTBY
 // Find all values in common between two lists, simple data types (int, decimal etc) checks values, comparer class to compare class objects
-
 int[] theNumbers = new int[] { 0,1,2,3,4 };
 int[] moreNumbers = new int[] { 3,4,5,6,7 };
 
 // Query syntax and method syntax
 resultIntList = (from num in theNumbers select num).Intersect(moreNumbers).ToList(); //Returns 3,4
 resultIntList = theNumbers.Intersect(moreNumbers).ToList(); //returns 3,4
+
+
+// IntersectBy allows you to define what part of the objects in your lists will be used to determine if they exist in both lists. Using a key selector.
+// The default data type for key expression is string, if you want to change to anything else you must pass two parameters, data type and key expression data type
+newList = (from g1 in ghostsList1 select g1)
+          .IntersectBy<Ghost, int>(
+            from g2 in ghostsList2 select g2.GhostAge,g1 => g1.GhostAge).ToList();
