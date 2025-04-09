3a9f12f3f81cc9d6b3db867b94130086f6c88cb0
Maekee <drmicke@gmail.com>
2025-04-09 19:06:10 +0200

    Update Linq.cs

--- Learning/Linq.cs
+++ Learning/Linq.cs
@@ -226,6 +226,12 @@ resultIntList = theNumbers.Intersect(moreNumbers).ToList(); //returns 3,4
 
 // IntersectBy allows you to define what part of the objects in your lists will be used to determine if they exist in both lists. Using a key selector.
 // The default data type for key expression is string, if you want to change to anything else you must pass two parameters, data type and key expression data type
+// Query syntax
+
 newList = (from g1 in ghostsList1 select g1)
           .IntersectBy<Ghost, int>(
             from g2 in ghostsList2 select g2.GhostAge,g1 => g1.GhostAge).ToList();
+
+// Method syntax
+newList = ghostsList1.IntersectBy<Ghost, int>(
+            ghostsList2.Select(g2 => g2.GhostAge), g1 => g1.GhostAge).ToList();
