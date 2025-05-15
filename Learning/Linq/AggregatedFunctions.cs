8efe0bec2336ed781952585425a052c0780c2102
Maekee <drmicke@gmail.com>
2025-05-15 09:45:04 +0200

    Update AggregatedFunctions.cs

--- Learning/Linq/AggregatedFunctions.cs
+++ Learning/Linq/AggregatedFunctions.cs
@@ -7,3 +7,9 @@ var daAverage = daNumbers.Average();
 
 // Used in combination with Where
 var daOnesFiltered = daNumbers.Where(n => n == 1).ToList().Count();
+
+// The Aggregate method will iterate through the daNumbers array.
+// The 'seed' value is set to decimal value 0 (0M), simply the starting value.
+// This seed value will be the starting point for our sum
+// After iterating through daNumbers array, the Aggregate method will return the final value of sum
+var daAggregateExample = daNumbers.Aggregate(0M, (sum, daNumberItem) => sum + daNumberItem);
